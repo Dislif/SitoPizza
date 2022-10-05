ef53f24d4a1a442a156ad4a0e1a70e74078083d8
Dislif <dislif77@virgilio.it>
2022-10-05 19:32:46 +0200

    Modificata la grafica e aggiunte funzioni php

--- phpinclude.php
+++ phpinclude.php
@@ -6,4 +6,26 @@ function test_input($data) {
   $data = htmlspecialchars($data);
   return $data;
 }
+
+
+function isin($vett, $el) {
+	sort($vett);
+    $l = count($vett);
+    while ($l > 1) {
+      $pivind = round($l/2);
+      $pivot = $vett[$pivind];
+      if ($el < $pivot) {
+          $vett = array_slice($vett, 0, $pivind);
+      } else {
+          $vett = array_slice($vett, $pivind, $l-$pivind+1);
+      }
+      $l = count($vett);
+    }
+    if ($el==$vett[0]) {
+      return 1;
+    } else {
+      return 0;
+    }
+}
+
 ?>
